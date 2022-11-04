cfa22b7ea92392b6edfff6f714d840a98d634c93
Zahid <rashidzahid819@gmail.com>
2022-11-04 22:48:33 +0530

    few changes

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+let homeScore=0
+let oppteamScore=0
+let startGame=false
+
+function start(){
+    startGame=true
+    homeScore=0
+    oppteamScore=0
+    playGame()
+
+    
+}
+
+document.getElementById("lll").addEventListener("click",function(){
+    startGame=false
+    console.log(startGame)
+    playGame()
+    
+    
+    
+
+})
+    
+function playGame(){
+
+    if (startGame===true){
+        document.getElementById("hbtn-1").addEventListener("click",function(){
+            homeScore++
+            render()
+            
+            
+        })
+    } else{
+        homeScore=0
+        render()
+    }
+    return
+    
+}
+
+function render(){
+        document.getElementById("hscore").innerText=homeScore
+        // document.getElementById("oscore").innerText=oppteamScore
+
+    
+}
